bfaf9768ad26aaa77d3545171cfe89f124870c7c
XBanTs <ayomideoyewo@gmail.com>
2025-08-19 15:41:35 +0100

    ""

--- main.py
+++ main.py
@@ -1,12 +1,46 @@
 from fastapi import FastAPI
+from typing import Optional
+from pydantic import BaseModel
+import uvicorn
 
 app = FastAPI()
 
 
-@app.get('/')
-def index():
-  return {'data': {'name': 'Ayo'}}
+@app.get('/blog')
+def index(limit=10, published : bool = True, sort: Optional[str] = None):
+  # only get 10 published blogs
+  if published:
+    return {'data': f'{limit} published blogs from the db'}
+  else:
+    return {'data': f'{limit} blogs from the db'}
 
-@app.get('/about')
-def about():
-  return {'data': 'about page'}
+@app.get('/blog/unpublished')
+def unpublished():
+  return {'data': 'all unpublished blogs'}
+
+
+@app.get('/blog/{id}')
+def show(id: int):
+  # fetch blog with id = id
+  return {'data': id}
+
+
+
+
+@app.get('/blog/{id}/comments')
+def comments(id, limit=10):
+  # fetch comments of blog with id = id
+  return {'data': {'1','2'}}
+
+
+class Blog(BaseModel):
+  title: str
+  body: str
+  published: Optional[bool]
+
+
+@app.post('/blog')
+def create_blog(blog: Blog):
+  return {'data': f"Blog is created with title as {blog.title}"}
+
+
